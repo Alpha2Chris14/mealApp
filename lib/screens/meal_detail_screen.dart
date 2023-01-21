41b94b5ed0ee6ae718c7b8680cfba9fb574fbf76
Christian <onyekachristian14@gmail.com>
2023-01-21 15:27:18 +0100

    made modification to meal detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -19,52 +19,69 @@ class MealDetailScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(selectedMeal.title)),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            child: Image.network(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.topCenter,
+            fit: BoxFit.contain,
+            image: NetworkImage(
               selectedMeal.imageUrl,
-              fit: BoxFit.fill,
             ),
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              selectedMeal.title,
-              style: Theme.of(context).textTheme.titleLarge,
+        ),
+        child: Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 2),
-            child: Text(
-              "Ingredent",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
+          margin:
+              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
+          elevation: 30,
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  selectedMeal.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 2),
+                child: Text(
+                  "Ingredent",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                ),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                padding: const EdgeInsets.all(10),
+                height: 200,
+                width: double.infinity,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Theme.of(context).colorScheme.background,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(selectedMeal.ingredients[index]),
+                      ),
+                    );
+                  },
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+              )
+            ],
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-            ),
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            height: 200,
-            width: 300,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Theme.of(context).colorScheme.background,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(selectedMeal.ingredients[index]),
-                  ),
-                );
-              },
-              itemCount: selectedMeal.ingredients.length,
-            ),
-          )
-        ],
+        ),
       ),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
