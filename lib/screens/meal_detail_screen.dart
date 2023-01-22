d189d1944c133ac06a94a85a2285562e25b689cb
Christian <onyekachristian14@gmail.com>
2023-01-22 16:22:24 +0100

    converted meal steps to listTile

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -30,77 +30,90 @@ class MealDetailScreen extends StatelessWidget {
             ),
           ),
         ),
-        child: Card(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+        child: SingleChildScrollView(
+          child: Card(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
             ),
-          ),
-          margin:
-              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
-          elevation: 30,
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  selectedMeal.title,
-                  style: Theme.of(context).textTheme.titleLarge,
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
+            elevation: 30,
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    selectedMeal.title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 2),
-                child: Text(
-                  "Ingredent",
-                  style: Theme.of(context).textTheme.titleMedium,
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 2),
+                  child: Text(
+                    "Ingredent",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.grey),
-                ),
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                padding: const EdgeInsets.all(10),
-                height: 200,
-                width: double.infinity,
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    return Card(
-                      color: Theme.of(context).colorScheme.background,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(selectedMeal.ingredients[index]),
-                      ),
-                    );
-                  },
-                  itemCount: selectedMeal.ingredients.length,
-                ),
-              ),
-              Container(
-                height: 200,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Text(
-                          "${index + 1} . ${selectedMeal.steps}",
-                          style: const TextStyle(fontSize: 16),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  padding: const EdgeInsets.all(10),
+                  height: 200,
+                  width: double.infinity,
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Card(
+                        color: Theme.of(context).colorScheme.background,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(selectedMeal.ingredients[index]),
                         ),
-                        const SizedBox(
-                          height: 10,
-                        )
-                      ],
-                    );
-                  },
-                  itemCount: selectedMeal.steps.length,
+                      );
+                    },
+                    itemCount: selectedMeal.ingredients.length,
+                  ),
                 ),
-              )
-            ],
+                Container(
+                  height: 200,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor:
+                              const Color.fromARGB(255, 179, 77, 197),
+                          foregroundColor: Colors.white,
+                          // foregroundColor: Theme.of(context).primaryColor,
+                          child: Text("${index + 1}"),
+                        ),
+                        // title: Text("Step ${index + 1}"),
+                        subtitle: Text("${selectedMeal.steps}"),
+                      );
+                      // return Column(
+                      //   children: [
+                      //     Text(
+                      //       "${index + 1} . ${selectedMeal.steps}",
+                      //       style: const TextStyle(fontSize: 16),
+                      //     ),
+                      //     const SizedBox(
+                      //       height: 10,
+                      //     )
+                      //   ],
+                      // );
+                    },
+                    itemCount: selectedMeal.steps.length,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
