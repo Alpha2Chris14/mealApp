a7ab7e5a3306679f75ea1922b4f8356f060cd947
Christian <onyekachristian14@gmail.com>
2023-01-24 17:16:45 +0100

    added is glutten free switch and function

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -4,19 +4,68 @@ import 'package:flutter/src/widgets/framework.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
-  static const routeName = "/filter";
+  static const routeName = "/filters";
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _gluteenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
 
   @override
   Widget build(BuildContext context) {
+    // _switchTiles("Gluten-Free","Only include gluten-free meals. ",_gluteenFree)
+    Widget _switchTiles(
+        String title, String subtitle, bool value, Function updateValue) {
+      return SwitchListTile(
+        activeColor: Colors.purple,
+        title: Text(title),
+        value: value,
+        onChanged: (value) => updateValue(value),
+        subtitle: Text(subtitle),
+      );
+    }
+
+    void _changeIsGluttenFree(value) {
+      setState(() {
+        _gluteenFree = value;
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Settings"),
       ),
       drawer: MainDrawer(),
-      body: const Center(child: Text("filters")),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Adjust your meal selection",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              _switchTiles(
+                "Gluten-Free",
+                "Only include gluten-free meals. ",
+                _gluteenFree,
+                _changeIsGluttenFree,
+              ),
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
