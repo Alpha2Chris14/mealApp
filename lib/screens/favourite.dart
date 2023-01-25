fb902e2189e79b179d4523f8acbd76ba51c53ef7
Christian <onyekachristian14@gmail.com>
2023-01-26 00:34:17 +0100

    added favourite page with details

--- lib/screens/favourite.dart
+++ lib/screens/favourite.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import '../models/meal.dart';
+import '../widgets/meal_item.dart';
 
 class Favourite extends StatelessWidget {
   List<Meal> favoriteMeals;
@@ -11,19 +12,36 @@ class Favourite extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Image.asset("assets/images/no-transactions.png"),
-          Text(
-            "No Favourite Yet",
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w400,
-                ),
-          )
-        ],
-      ),
-    );
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Column(
+          children: [
+            Image.asset("assets/images/no-transactions.png"),
+            Text(
+              "No Favourite Yet",
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w400,
+                  ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            duration: favoriteMeals[index].duration,
+            deleteItem: () {},
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
